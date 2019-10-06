c511e1495624dfe0fc4ce8bfbff1ed67b5b664ae
Константин Дьяченко <konard@me.com>
2019-10-06 12:13:54 +0300

    Code cleanup and refactoring.

--- Platform.Collections.Methods.Console/Console.cpp
+++ Platform.Collections.Methods.Console/Console.cpp
@@ -1,6 +1,3 @@
-// CppConsoleExample.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <unordered_set>
 #include <chrono>
@@ -43,33 +40,6 @@ public:
 #include "SizedAndThreadedAVLBalancedTree.cpp"
 #include "TestExtensions.cpp"
 //#include "TreesTests.cpp"
-
-// template <typename TElement> class SizedBinaryTreeMethodsBase : GenericCollectionMethodsBase<TElement>
-template<typename TElement> class A // : A<TElement>
-{
-public:
-    virtual TElement* GetLeftReference(TElement node) = 0;
-    virtual void AttachCore(TElement* root, TElement node) = 0;
-};
-
-// template <typename TElement> class SizeBalancedTreeMethods : SizedBinaryTreeMethodsBase<TElement>
-template<typename TElement> class B : public A<TElement>
-{
-public:
-    void AttachCore(TElement* root, TElement node) override
-    {
-        this->GetLeftReference(0);
-    }
-    //virtual TElement* GetLeftReference(TElement node) override = 0;
-};
-
-// template <typename TElement, std::size_t N> class SizeBalancedTree : Platform::Collections::Methods::Trees::SizeBalancedTreeMethods<TElement>
-template<typename TElement, std::size_t N> class C : public B<TElement>
-{
-public:
-    TElement* GetLeftReference(TElement node) override { return nullptr; }
-};
-
 
 int main()
 {
@@ -118,45 +88,22 @@ int main()
             return avlTree.GetCount();
         }
     };
-    //X::Allocate();
-    //auto* pointer = &X::Allocate;
-    //C<std::uint32_t, 10000> c;
-    //c.AttachCore(nullptr, 0);
-    //Platform::Collections::Methods::Tests::SizeBalancedTree2<std::uint32_t, 10000> sizeBalancedTree2;
-    //Platform::Collections::Methods::Tests::SizedAndThreadedAVLBalancedTree<std::uint32_t, 10000> avlTree;
-
-    //Platform::Collections::Methods::Trees::SizeBalancedTreeMethods<std::uint32_t>* sbt = &sizeBalancedTree;
-    //Platform::Collections::Methods::Trees::SizedBinaryTreeMethodsBase<std::uint32_t>* base = sbt;
 
     auto t1 = std::chrono::high_resolution_clock::now();
     Platform::Collections::Methods::Tests::TestExtensions::TestMultipleCreationsAndDeletions<std::uint32_t>(sizeBalancedTree, &sbtWrappers::Allocate, &sbtWrappers::Free, &sizeBalancedTree.Root, &sbtWrappers::GetCount, 2000);
     auto t2 = std::chrono::high_resolution_clock::now();
     auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-    std::cout << duration1 << "\n";
+    std::cout << duration1 << "ms for SizeBalancedTreeMethods\n";
 
     auto t3 = std::chrono::high_resolution_clock::now();
     Platform::Collections::Methods::Tests::TestExtensions::TestMultipleCreationsAndDeletions<std::uint32_t>(sizeBalancedTree2, &sbt2Wrappers::Allocate, &sbt2Wrappers::Free, &sizeBalancedTree2.Root, &sbt2Wrappers::GetCount, 2000);
     auto t4 = std::chrono::high_resolution_clock::now();
     auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t3).count();
-    std::cout << duration2 << "\n";
+    std::cout << duration2 << "ms for SizeBalancedTreeMethods2\n";
 
     auto t5 = std::chrono::high_resolution_clock::now();
     Platform::Collections::Methods::Tests::TestExtensions::TestMultipleCreationsAndDeletions<std::uint32_t>(avlTree, &avl2Wrappers::Allocate, &avl2Wrappers::Free, &avlTree.Root, &avl2Wrappers::GetCount, 2000);
     auto t6 = std::chrono::high_resolution_clock::now();
     auto duration3 = std::chrono::duration_cast<std::chrono::milliseconds>(t6 - t5).count();
-    std::cout << duration3 << "\n";
-
-    Assert::Equal(1, 1);
-    std::cout << "Hello World!\n";
+    std::cout << duration3 << "ms SizedAndThreadedAVLBalancedTreeMethods\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
