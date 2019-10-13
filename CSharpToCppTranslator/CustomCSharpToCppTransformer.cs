c9967d6f9c9d6c208f96c8ae8888d2f9feef2835
Константин Дьяченко <konard@me.com>
2019-10-13 09:44:31 +0300

    Using CSharpToCppTransformer again.

--- CSharpToCppTranslator/CustomCSharpToCppTransformer.cs
+++ CSharpToCppTranslator/CustomCSharpToCppTransformer.cs
@@ -7,8 +7,9 @@ using Platform.RegularExpressions.Transformer.CSharpToCpp;
 
 namespace CSharpToCppTranslator
 {
-    public class CustomCSharpToCppTransformer : Transformer // CSharpToCppTransformer
+    public class CustomCSharpToCppTransformer : CSharpToCppTransformer // Transformer
     {
+        /*
         public static readonly IList<ISubstitutionRule> FirstStage = new List<SubstitutionRule>
         {
             // // ...
@@ -159,6 +160,7 @@ namespace CSharpToCppTranslator
             // GetElement(node)->Right
             (new Regex(@"([a-zA-Z0-9]+)\(([a-zA-Z0-9\*]+)\)\.([a-zA-Z0-9]+)"), "$1($2)->$3", null, 0),
         }.Cast<ISubstitutionRule>().ToList();
+        */
 
         public static readonly IList<ISubstitutionRule> Rules = new List<SubstitutionRule>
         {   
@@ -302,6 +304,7 @@ namespace CSharpToCppTranslator
             (new Regex(@"\(SizedBinaryTreeMethodsBase<TElement>"), "(Platform::Collections::Methods::Trees::SizedBinaryTreeMethodsBase<TElement>&", new Regex(@"TestExtensions\.cs"), 0),
         }.Cast<ISubstitutionRule>().ToList();
 
+        /*
         public static readonly IList<ISubstitutionRule> LastStage = new List<SubstitutionRule>
         {
             // (expression)
@@ -332,9 +335,10 @@ namespace CSharpToCppTranslator
             // class
             (new Regex(@"(\S[\r\n]{1,2})?[\r\n]+class"), "$1class", null, 0),
         }.Cast<ISubstitutionRule>().ToList();
+        */
 
-        //public CustomCSharpToCppTransformer() : base(Rules) { }
+        public CustomCSharpToCppTransformer() : base(Rules) { }
 
-        public CustomCSharpToCppTransformer() : base(FirstStage.Concat(Rules).Concat(LastStage).ToList()) { }
+        //public CustomCSharpToCppTransformer() : base(FirstStage.Concat(Rules).Concat(LastStage).ToList()) { }
     }
 }
