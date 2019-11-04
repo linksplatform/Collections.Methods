c49bba0bedc5cfcdd0bb3879845b3bba9e865797
Константин Дьяченко <konard@me.com>
2019-11-04 18:34:23 +0300

    Attempt to use any value.

--- push-cpp-nuget.sh
+++ push-cpp-nuget.sh
@@ -6,8 +6,8 @@ sudo apt-get install xmlstarlet
 NuSpecFile=$(echo cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary.*.nuspec)
 echo "$NuSpecFile"
 xmlstarlet el -u "$NuSpecFile"
-xmlstarlet sel -t -m '///version[1]' -v . -n "$NuSpecFile"
-Version=$(xmlstarlet sel -t -m '///version[1]' -v . -n <"$NuSpecFile")
+xmlstarlet sel -t -m '//version' -v . "$NuSpecFile"
+Version=$(xmlstarlet sel -t -m '//version' -v . "$NuSpecFile")
 echo "$Version"
 
 #Version=$(xmlstarlet sel -t -m '//VersionPrefix[1]' -v . -n <"Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.csproj")
