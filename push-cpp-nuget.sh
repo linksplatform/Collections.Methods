0f3548e14690c1cc88d5a286cb276543d12477c8
Константин Дьяченко <konard@me.com>
2019-11-04 18:52:01 +0300

    Using different way to get current cpp NuGet package version.

--- push-cpp-nuget.sh
+++ push-cpp-nuget.sh
@@ -3,38 +3,15 @@ set -e # Exit with nonzero exit code if anything fails
 
 sudo apt-get install xmlstarlet
 
-NuSpecFile=$(echo cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary.*.nuspec)
-echo "$NuSpecFile"
-
-cp "$NuSpecFile" "x.xml"
-
-xmlstarlet el -u x.xml
-xmlstarlet sel -t -m '/package' -v . x.xml
-Version=$(xmlstarlet sel -t -m '/package' -v . x.xml)
-echo "$Version"
-
-#Version=$(xmlstarlet sel -t -m '//VersionPrefix[1]' -v . -n <"Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.csproj")
-
 # Get version string
-#PackageFileNamePrefix="Platform.$REPOSITORY_NAME/bin/Release/Platform.$REPOSITORY_NAME."
-#PackageFileNameSuffix=".nupkg"
-#PackageFileName=$(echo "$PackageFileNamePrefix"*"$PackageFileNameSuffix")
-#Version="${PackageFileName#$PackageFileNamePrefix}"
-#Version="${Version%$PackageFileNameSuffix}"
-
-#OldVersion=$(xmlstarlet sel -t -m '//version[1]' -v . -n <"cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary.*.nuspec")
-#xmlstarlet edit --inplace --update '/preferences/options/dbUser' --value 123 preferences.xml
-
-# Pack NuGet package
-# dotnet pack -c Release
-cp cpp/Platform.$REPOSITORY_NAME/*.h "cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/lib/native/include/"
-cp cpp/Platform.$REPOSITORY_NAME/*.cpp "cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/lib/native/include/"
-echo "Files copied."
-
-nuget pack "$NuSpecFile"
+PackageSpecFileNamePrefix="cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary."
+PackageSpecFileNameSuffix=".nuspec"
+PackageSpecFileName=$(echo "$PackageSpecFileNamePrefix"*"$PackageSpecFileNameSuffix")
+Version="${PackageSpecFileName#$PackageSpecFileNamePrefix}"
+Version="${Version%$PackageSpecFileNameSuffix}"
 
 # Ensure NuGet package does not exist
-NuGetPackageUrl="https://globalcdn.nuget.org/packages/Platform.$REPOSITORY_NAME.TemplateLibrary.$Version$PackageFileNameSuffix"
+NuGetPackageUrl="https://globalcdn.nuget.org/packages/Platform.$REPOSITORY_NAME.TemplateLibrary.$Version.nupkg"
 NuGetPackageUrl=$(echo "$NuGetPackageUrl" | tr '[:upper:]' '[:lower:]')
 NuGetPageStatus="$(curl -Is "$NuGetPackageUrl" | head -1)"
 StatusContents=( $NuGetPageStatus )
@@ -42,6 +19,31 @@ if [ "${StatusContents[1]}" == "200" ]; then
   echo "NuGet with current version is already pushed."
   exit 0
 fi
+
+#NuSpecFile=$(echo cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary.*.nuspec)
+#echo "$NuSpecFile"
+
+#cp "$NuSpecFile" "x.xml"
+#
+#xmlstarlet el -u x.xml
+#xmlstarlet sel -t -m '/package' -v . x.xml
+#Version=$(xmlstarlet sel -t -m '/package' -v . x.xml)
+#echo "$Version"
+
+#Version=$(xmlstarlet sel -t -m '//VersionPrefix[1]' -v . -n <"Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.csproj")
+
+
+
+#OldVersion=$(xmlstarlet sel -t -m '//version[1]' -v . -n <"cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary.*.nuspec")
+#xmlstarlet edit --inplace --update '/preferences/options/dbUser' --value 123 preferences.xml
+
+# Pack NuGet package
+# dotnet pack -c Release
+cp "cpp/Platform.$REPOSITORY_NAME/"*.h "cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/lib/native/include/"
+cp "cpp/Platform.$REPOSITORY_NAME/"*.cpp "cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/lib/native/include/"
+echo "Files copied."
+
+nuget pack "$PackageSpecFileName"
 
 # Push NuGet package
 # dotnet nuget push ./**/*.nupkg -s https://api.nuget.org/v3/index.json -k "${NUGETTOKEN}" 
