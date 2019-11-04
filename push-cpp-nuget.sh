8c22b88e058c6c119a2155d2048b77037c3ad985
Константин Дьяченко <konard@me.com>
2019-11-04 19:14:05 +0300

    Comments code removed from script.

--- push-cpp-nuget.sh
+++ push-cpp-nuget.sh
@@ -20,26 +20,6 @@ if [ "${StatusContents[1]}" == "200" ]; then
   exit 0
 fi
 
-#NuSpecFile=$(echo cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary.*.nuspec)
-#echo "$NuSpecFile"
-
-#cp "$NuSpecFile" "x.xml"
-#
-#xmlstarlet el -u x.xml
-#xmlstarlet sel -t -m '/package' -v . x.xml
-#Version=$(xmlstarlet sel -t -m '/package' -v . x.xml)
-#echo "$Version"
-
-#Version=$(xmlstarlet sel -t -m '//VersionPrefix[1]' -v . -n <"Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.csproj")
-
-
-
-#OldVersion=$(xmlstarlet sel -t -m '//version[1]' -v . -n <"cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary.*.nuspec")
-#xmlstarlet edit --inplace --update '/preferences/options/dbUser' --value 123 preferences.xml
-
-# Pack NuGet package
-# dotnet pack -c Release
-
 LibNativeIncludeDirectory="cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/lib/native/include"
 
 mkdir -p "$LibNativeIncludeDirectory"
@@ -51,7 +31,6 @@ echo "Files copied."
 nuget pack "$PackageSpecFileName"
 
 # Push NuGet package
-# dotnet nuget push ./**/*.nupkg -s https://api.nuget.org/v3/index.json -k "${NUGETTOKEN}" 
 nuget push ./**/*.nupkg -NoSymbols -Source https://api.nuget.org/v3/index.json -ApiKey "${NUGETTOKEN}" 
 
 # Clean up
