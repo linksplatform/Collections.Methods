0ca60fde8884a04049caf41b7338b5567d730367
Константин Дьяченко <konard@me.com>
2019-11-04 18:56:21 +0300

    Install nuget.

--- push-cpp-nuget.sh
+++ push-cpp-nuget.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-sudo apt-get install xmlstarlet
+sudo apt install nuget
 
 # Get version string
 PackageSpecFileNamePrefix="cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary."
