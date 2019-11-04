c2a3d7daa8a979520acaf0d06ae86ec011f6bc14
Константин Дьяченко <konard@me.com>
2019-11-04 18:23:36 +0300

    Attempt to increase deepness.

--- push-cpp-nuget.sh
+++ push-cpp-nuget.sh
@@ -5,8 +5,8 @@ sudo apt-get install xmlstarlet
 
 NuSpecFile=$(echo cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary.*.nuspec)
 echo "$NuSpecFile"
-xmlstarlet sel -t -m '//version[1]' -v . -n <"$NuSpecFile"
-Version=$(xmlstarlet sel -t -m '//version[1]' -v . -n <"$NuSpecFile")
+xmlstarlet sel -t -m '///version[1]' -v . -n <"$NuSpecFile"
+Version=$(xmlstarlet sel -t -m '///version[1]' -v . -n <"$NuSpecFile")
 echo "$Version"
 
 #Version=$(xmlstarlet sel -t -m '//VersionPrefix[1]' -v . -n <"Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.csproj")
