a08da49640c48a21fbc8c465a72d30e2f7fec513
Konstantin Dyachenko <drakonard@gmail.com>
2020-10-12 15:58:39 +0300

    [C#] 0.4.0, [C++] 0.2.0

--- cpp/Platform.Collections.Methods/Platform.Collections.Methods.h
+++ cpp/Platform.Collections.Methods/Platform.Collections.Methods.h
@@ -8,8 +8,12 @@
 #include "GenericCollectionMethodsBase.h"
 
 #include "Lists/DoublyLinkedListMethodsBase.h"
-#include "Lists/CircularDoublyLinkedListMethods.h"
-#include "Lists/OpenDoublyLinkedListMethods.h"
+#include "Lists/AbsoluteDoublyLinkedListMethodsBase.h"
+#include "Lists/AbsoluteCircularDoublyLinkedListMethods.h"
+#include "Lists/AbsoluteOpenDoublyLinkedListMethods.h"
+#include "Lists/RelativeDoublyLinkedListMethodsBase.h"
+#include "Lists/RelativeCircularDoublyLinkedListMethods.h"
+#include "Lists/RelativeOpenDoublyLinkedListMethods.h"
 
 #include "Trees/SizedBinaryTreeMethodsBase.h"
 #include "Trees/RecursionlessSizeBalancedTreeMethods.h"
