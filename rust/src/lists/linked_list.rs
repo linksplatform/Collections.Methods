1476a73b890f57ad96b2131a4575d05d36b983b6
dodic <dodickgod@gmail.com>
2022-07-27 16:45:19 +0300

    Use submodule

--- rust/src/lists/linked_list.rs
+++
@@ -1,9 +0,0 @@
-use platform_data::LinkType;
-
-pub trait LinkedList<T: LinkType> {
-    fn get_previous(&self, element: T) -> T;
-    fn get_next(&self, element: T) -> T;
-
-    fn set_previous(&mut self, element: T, previous: T);
-    fn set_next(&mut self, element: T, next: T);
-}
--- rust/src/trees/mod.rs
+++
@@ -1,5 +0,0 @@
-mod no_recur_szb_tree;
-mod szb_tree;
-
-pub use no_recur_szb_tree::NoRecurSzbTree;
-pub use szb_tree::SzbTree;
