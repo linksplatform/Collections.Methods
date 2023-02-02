d4570014f3dc44819d702e4e8dd7e903664f7e90
Daulet Nassipkali <uralskdev@gmail.com>
2023-02-02 19:36:49 +0500

    Update RelativeDoublyLinkedListMethodsBase.h

--- cpp/Platform.Collections.Methods/Lists/RelativeDoublyLinkedListMethodsBase.h
+++ cpp/Platform.Collections.Methods/Lists/RelativeDoublyLinkedListMethodsBase.h
@@ -3,8 +3,8 @@
     template <class TSelf, typename ...> class RelativeDoublyLinkedListMethodsBase;
     template <class TSelf, typename TElement> class RelativeDoublyLinkedListMethodsBase<TSelf, TElement> : public DoublyLinkedListMethodsBase<TSelf, TElement>
     {
-        protected: void IncrementSize(TElement headElement) { object().SetSize(headElement, object().GetSize(headElement) + 1); }
+        protected: void IncrementSize(TElement headElement) { this->object().SetSize(headElement, this->object().GetSize(headElement) + 1); }
 
-        protected: void DecrementSize(TElement headElement) { object().SetSize(headElement, object().GetSize(headElement) - 1); }
+        protected: void DecrementSize(TElement headElement) { this->object().SetSize(headElement, this->object().GetSize(headElement) - 1); }
     };
 }
