00c6607df82088546f4dc775e030737a10ad37d1
Daulet Nassipkali <uralskdev@gmail.com>
2023-02-02 19:35:08 +0500

    Update RelativeDoublyLinkedListMethodsBase.h

--- cpp/Platform.Collections.Methods/Lists/RelativeDoublyLinkedListMethodsBase.h
+++ cpp/Platform.Collections.Methods/Lists/RelativeDoublyLinkedListMethodsBase.h
@@ -1,22 +1,8 @@
 ﻿namespace Platform::Collections::Methods::Lists
 {
-    template <class impl_t, typename ...> class RelativeDoublyLinkedListMethodsBase;
-    template <class impl_t, typename TElement> class RelativeDoublyLinkedListMethodsBase<impl_t, TElement> : public DoublyLinkedListMethodsBase<impl_t, TElement>
+    template <class TSelf, typename ...> class RelativeDoublyLinkedListMethodsBase;
+    template <class TSelf, typename TElement> class RelativeDoublyLinkedListMethodsBase<TSelf, TElement> : public DoublyLinkedListMethodsBase<TSelf, TElement>
     {
-        public: using Polymorph<impl_t>::object;
-
-        protected: TElement GetFirst(TElement headElement) { return object().GetFirst(headElement); };
-
-        protected: TElement GetLast(TElement headElement) { return object().GetLast(headElement); };
-
-        protected: TElement GetSize(TElement headElement) { return object().GetSize(headElement); };
-
-        protected: void SetFirst(TElement headElement, TElement element) { object().SetFirst(headElement, element); };
-
-        protected: void SetLast(TElement headElement, TElement element) { object().SetLast(headElement, element); };
-
-        protected: void SetSize(TElement headElement, TElement size) { object().SetSize(headElement, size); };
-
         protected: void IncrementSize(TElement headElement) { object().SetSize(headElement, object().GetSize(headElement) + 1); }
 
         protected: void DecrementSize(TElement headElement) { object().SetSize(headElement, object().GetSize(headElement) - 1); }
